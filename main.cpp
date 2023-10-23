98eb7bacd0d73d8654bc2f58db16cb4427e03fd0
Kiki <kikitheheal@gmail.com>
2023-10-23 17:29:36 +0200

    fehlerfreie berechnung und dauerschleife dass ich immer wieder berechnen kann

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <limits>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 int main() {
     while (true) {
-        double zahl1 , zahl2 = 0, ergebnis = 0;
+        double zahl1 = 0 , zahl2 = 0, ergebnis = 0;
         char rechenzeichen;
 
         cout << "\n Gib eine Rechenaufgabe ein oder beende das Programm mit 'exit': ";
-        cin >> zahl1 >> rechenzeichen >> zahl2;
+        string eingabe;
+        cin >> eingabe;
 
-        /* Beenden Überprüfen
-        if (zahl1 == 'exit') {
+        // Beenden Überprüfen
+        if (eingabe == "exit") {
             break;
-        } */
-
-
+        }
 
         // ( Input prüft ob zahl) wenn nicht dann Fehlermeldung
-        if (!(cin >> zahl1) || !(cin >> rechenzeichen) || !(cin >> zahl2)) {
+        istringstream eingabeStream(eingabe);
+        if (!(eingabeStream >> zahl1 >> rechenzeichen >> zahl2)) {
             cout << "Ungültige Eingabe, bitte erneut eingeben.\n";
             cin.clear(); // wird verwendet um den Fehlerzustand zurückzusetzen
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); // löscht jeglichen input der bei zahl1 war
