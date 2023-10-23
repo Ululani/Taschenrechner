7ed7143e82c3bfa1a4562c86183882c491f44599
Kiki <kikitheheal@gmail.com>
2023-10-23 11:00:14 +0200

    erweiterungsversuch um das Programm zu verbessern, es soll fehlerhafte eingaben vom user abblocken

--- main.cpp
+++ main.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 int main() {
-    double zahl1, zahl2, ergebnis;
-    char rechenzeichen;
+    while (true) {
+        double zahl1 , zahl2 = 0, ergebnis = 0;
+        char rechenzeichen;
 
-    cout << "Gib eine Rechenaufgabe ein: ";
-    cin >> zahl1 >> rechenzeichen >> zahl2;
+        cout << "\n Gib eine Rechenaufgabe ein oder beende das Programm mit 'exit': ";
+        cin >> zahl1 >> rechenzeichen >> zahl2;
 
-    switch (rechenzeichen) {
-        case '+':
-            ergebnis = zahl1 + zahl2;
+        /* Beenden Überprüfen
+        if (zahl1 == 'exit') {
             break;
-        case '-':
-            ergebnis = zahl1 - zahl2;
-            break;
-        case '*':
-            ergebnis = zahl1 * zahl2;
-            break;
-        case '/':
-            ergebnis = zahl1 / zahl2;
-            break;
+        } */
 
-        default:
-            cout << "unbekanntes Rechenzeichen\n";
-            return 1;
+
+
+        // ( Input prüft ob zahl) wenn nicht dann Fehlermeldung
+        if (!(cin >> zahl1) || !(cin >> rechenzeichen) || !(cin >> zahl2)) {
+            cout << "Ungültige Eingabe, bitte erneut eingeben.\n";
+            cin.clear(); // wird verwendet um den Fehlerzustand zurückzusetzen
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // löscht jeglichen input der bei zahl1 war
+            continue;
+        }
+
+
+        switch (rechenzeichen) {
+            case '+':
+                ergebnis = zahl1 + zahl2;
+                break;
+            case '-':
+                ergebnis = zahl1 - zahl2;
+                break;
+            case '*':
+                ergebnis = zahl1 * zahl2;
+                break;
+            case '/':
+                ergebnis = zahl1 / zahl2;
+                break;
+
+            default:
+                cout << "unbekanntes Rechenzeichen\n";
+                return 1;
+        }
+
+        cout << zahl1 << " " << rechenzeichen << " " << zahl2 << " = " << ergebnis << " \n";
     }
-
-    cout << zahl1 << rechenzeichen << zahl2 << " = " << ergebnis;
 }
